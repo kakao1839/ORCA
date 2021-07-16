07cad0084eb232d8bf20efbbfe12c7edddcab7a9
kakao <kwt20018@gmail.com>
2021-07-16 16:33:42 +0900

    add Auth-mail

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,17 +5,18 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('メールアドレスを確認してください') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('新しい認証メールがあなたのメールアドレスに送信されました。') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                    {{ __('あなたのメールアドレスに認証メールを送信しました。') }}
+                        <br>
+                    {{ __('認証メールを受け取れない場合') }}, <a href="{{ route('verification.resend') }}">{{ __('こちらのリンクをクリック') }}</a>.
                 </div>
             </div>
         </div>
